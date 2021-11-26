5d34ec867d1233f9129a5fbc8d25a019f84d5233
alonzo <alonzoalinch@gmail.com>
2021-11-26 17:15:58 -0500

    initial commit

---
+++ src/main/java/challenge/ravn/RavnApplication.java
@@ -0,0 +1,13 @@
+package challenge.ravn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavnApplication.class, args);
+    }
+
+}
